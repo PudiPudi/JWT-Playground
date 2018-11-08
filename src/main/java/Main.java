41d30c99b6f4047b74e17ebefff72f963a246983
Unknown <ludovic.marchand@hotmail.be>
2018-11-08 20:39:18 +0100

    Build RSAKey from RSA JWK

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,11 @@
+import com.nimbusds.jose.Algorithm;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.KeyType;
+import com.nimbusds.jose.jwk.RSAKey;
 import model.RSAPair;
+
+import java.util.Optional;
 
 public class Main {
 
@@ -10,6 +17,27 @@ public class Main {
 
         // TODO: verify using auth0
 //        Auth0.verify(rsaPair);
-        JJWT.verify(rsaPair);
+        // JJWT.verify(rsaPair);
+
+
+        JWK jwk = rsaPair.jwk;
+        JWKSet set = new JWKSet(jwk);
+        String jwkString = set.toJSONObject(false).toString();
+        System.out.println(jwkString);
+
+        // Set from string
+        JWKSet newSet = JWKSet.parse(jwkString);
+        Optional<JWK> newKeyOptional = newSet.getKeys().stream().findFirst();
+
+        if (!newKeyOptional.isPresent()) {
+            throw new Exception("No JWKs present");
+        }
+
+        JWK newJWK = newKeyOptional.get();
+        if (newJWK.getKeyType().equals(KeyType.RSA)) {
+            RSAKey rsaKey = RSAKey.parse(jwk.toJSONObject());
+
+            System.out.println(rsaKey.toPublicKey().toString());
+        }
     }
 }
